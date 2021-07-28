c59f5cdb021702cbd78d3da22390fe6781ebae0b
Mehak Zehra <mehak.rizvi.786@gmail.com>
2021-07-28 00:57:11 -0700

    saved cities to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var apiKey = "310760b7d84d28ecd4392ab3f7177bc1";
+var storedcitiesArr = [];
 var fetchCityForcast = function (cityName,lat,lon) {
     var WeatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" +lon +"&appid=" + apiKey;
     fetch(WeatherApi)
@@ -31,13 +32,23 @@ var displayfiveDayForecast = function(data){
 
     for(var index = 0; index < 5; index++){
 
-        var myDate = new Date(data.daily[index].dt*1000);
-        console.log(myDate.toLocaleString());
 
-        console.log("Temp:" +data.daily[index].temp.day);
-        console.log("Humidity:" +data.daily[index].humidity);
-        console.log("Wind:" +data.daily[index].wind_speed);
-        console.log("It's going to be: " +data.daily[index].weather[0].main);
+        var myDate = new Date(data.daily[index].dt*1000);
+        var dateEl = document.getElementById("date-" +index);
+        dateEl.textContent = myDate.toLocaleDateString();
+
+        var weatherForecast = document.getElementById("icon-"+index);
+        weatherForecast.textContent = data.daily[index].weather[0].main;
+
+        var tempEl = document.getElementById("temp-" +index);
+        tempEl.textContent = data.daily[index].temp.day;
+
+
+        var windEl = document.getElementById("wind-" +index);
+        windEl.textContent = data.daily[index].wind_speed;
+
+        var humidityEl = document.getElementById("humidity-" +index);
+        humidityEl.textContent = data.daily[index].humidity;
     } 
 
 }
@@ -55,10 +66,50 @@ var fetchCity = function (cityName) {
             return fetchCityForcast(cityName,lat,lon);
 
         });
+        
 }
 
 $(".search-btn").on("click", function () {
     var cityName = document.getElementById("city").value;
-    fetchCity(cityName);
+    if(cityName === ""){
+
+    }
+    else {
+        fetchCity(cityName);
+        recentlySearchedCities(cityName);
+    }
+    
 });
 
+var recentlySearchedCities= function(cityName) {
+    // get stored city array from local storage
+    var storedcitiesArr = JSON.parse(localStorage.getItem("city")); // []
+    if(!storedcitiesArr){
+        storedcitiesArr = [];
+    }
+
+    storedcitiesArr.push(cityName);//["London"]
+    localStorage.setItem("city", JSON.stringify(storedcitiesArr));//"London"
+
+    displaySearchedCities();
+}
+
+var displaySearchedCities = function(){
+
+    var cityHistoryEl = document.querySelector(".city-history");
+    cityHistoryEl.innerHTML = "";
+    var city = JSON.parse(localStorage.getItem("city"));
+    if(!city){
+        city = [];
+    }
+    for(var i = 0; i < city.length; i++){
+        var buttonEl = document.createElement("button");
+        buttonEl.className = "btn btn-primary btn-lg"
+        buttonEl.type = "button"
+        buttonEl.innerText = city[i];
+        cityHistoryEl.appendChild(buttonEl);
+        
+    }
+}
+
+displaySearchedCities();
